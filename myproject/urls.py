00d84466b76e8e19b64a3b55a4d7101bec13d73e
Ahmed Shahzad Kayani <110466702+AhmedKayani@users.noreply.github.com>
2024-05-25 13:33:24 +0500

    Delete myproject directory

--- myproject/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import redirect_to_login
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('authapp.urls')),
-    path('', redirect_to_login),
-]
--- myproject/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import redirect
-
-def redirect_to_login(request):
-    return redirect('/auth/login/')
