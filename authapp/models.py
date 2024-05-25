53b0776f40d7b778586c3915d41b23a7a181c0f1
Ahmed Shahzad Kayani <110466702+AhmedKayani@users.noreply.github.com>
2024-05-25 13:33:11 +0500

    Delete authapp directory

--- authapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Model for the user profile which is the name of the person and their networth
-class UserProfile(models.Model):
-    name = models.CharField(max_length=100)
-    networth = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return self.name
