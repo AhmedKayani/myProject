6d1935192312fbf2f7aca0c43065943f106c00a5
Ahmed Shahzad Kayani <110466702+AhmedKayani@users.noreply.github.com>
2024-05-25 15:15:48 +0500

    Updated code so data is visible in admin panel

--- crudApp/urls.py
+++ crudApp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from . import views
+from .views import signup_view, login_view, logout_view, home_view, update_home, delete_home
 
 urlpatterns = [
-  path('signup/', views.signup_view, name='signup'),
-  path('login/', views.login_view, name='login'),
-  path('logout/', views.logout_view, name='logout'),
-  path('home/', views.home_view, name='home'),
-  path('home/update/<int:profile_id>/', views.update_home, name='update'),
-  path('home/delete/<int:profile_id>/', views.delete_home, name='delete'), 
+  path('signup/', signup_view, name='signup'),
+  path('login/', login_view, name='login'),
+  path('logout/', logout_view, name='logout'),
+  path('home/', home_view, name='home'),
+  path('home/update/<int:profile_id>/', update_home, name='update'),
+  path('home/delete/<int:profile_id>/', delete_home, name='delete'), 
 ]
